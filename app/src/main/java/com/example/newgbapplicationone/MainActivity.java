e1c8afbcf149e5911e1c06d97b60e4fb4f151156
FaraKrik <googi13011@gmail.com>
2021-07-25 13:40:02 +0300

    Homework for lesson 5

--- app/src/main/java/com/example/newgbapplicationone/MainActivity.java
+++ app/src/main/java/com/example/newgbapplicationone/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.newgbapplicationone;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.MessageFormat;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,20 +31,55 @@ public class MainActivity extends AppCompatActivity {
     TextView operationField;
     Double operand = null;
     String lastOperation = "=";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        resultField =(TextView) findViewById(R.id.resultField);
+        resultField = (TextView) findViewById(R.id.resultField);
         numberField = (EditText) findViewById(R.id.numberField);
         operationField = (TextView) findViewById(R.id.operationField);
+
+        initView();
+
+
     }
+
+
+
+
+    public void buttonClick(View view){
+        int red= new Random().nextInt(255);
+        int green= new Random().nextInt(255);
+        int blue= new Random().nextInt(255);
+        ThemeColors.setNewThemeColor(MainActivity.this, red, green, blue);
+    }
+
+    private void initView() {
+
+        EditText editText = findViewById(R.id.numberField);
+        TextView textView = findViewById(R.id.textView);
+
+        findViewById(R.id.button18).setOnClickListener(v -> {
+            String name = editText.getText().toString();
+            textView.setText("Операция прошла успешна!");
+        });
+        findViewById(R.id.button18).setOnClickListener(v -> {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+        });
+
+    }
+
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putString("OPERATION", lastOperation);
-        if(operand!=null)
+        if (operand != null)
             outState.putDouble("OPERAND", operand);
         super.onSaveInstanceState(outState);
     }
@@ -38,32 +88,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         lastOperation = savedInstanceState.getString("OPERATION");
-        operand= savedInstanceState.getDouble("OPERAND");
+        operand = savedInstanceState.getDouble("OPERAND");
         resultField.setText(operand.toString());
         operationField.setText(lastOperation);
     }
 
-    public void onNumberClick(View view){
+    public void onNumberClick(View view) {
 
-        Button button = (Button)view;
+        Button button = (Button) view;
         numberField.append(button.getText());
 
-        if(lastOperation.equals("=") && operand!=null){
+        if (lastOperation.equals("=") && operand != null) {
             operand = null;
         }
     }
 
-    public void onOperationClick(View view){
+    public void onOperationClick(View view) {
 
-        Button button = (Button)view;
+        Button button = (Button) view;
         String op = button.getText().toString();
         String number = numberField.getText().toString();
 
-        if(number.length()>0){
+        if (number.length() > 0) {
             number = number.replace(',', '.');
-            try{
+            try {
                 performOperation(Double.valueOf(number), op);
-            }catch (NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 numberField.setText("");
             }
         }
@@ -71,39 +121,40 @@ public class MainActivity extends AppCompatActivity {
         operationField.setText(lastOperation);
     }
 
-    private void performOperation(Double number, String operation){
+    private void performOperation(Double number, String operation) {
 
-        if(operand ==null){
+        if (operand == null) {
             operand = number;
-        }
-        else{
-            if(lastOperation.equals("=")){
+        } else {
+            if (lastOperation.equals("=")) {
                 lastOperation = operation;
             }
-            switch(lastOperation){
+            switch (lastOperation) {
                 case "=":
-                    operand =number;
+                    operand = number;
                     break;
                 case "/":
-                    if(number==0){
-                        operand =0.0;
-                    }
-                    else{
-                        operand /=number;
+                    if (number == 0) {
+                        operand = 0.0;
+                    } else {
+                        operand /= number;
                     }
                     break;
                 case "*":
-                    operand *=number;
+                    operand *= number;
                     break;
                 case "+":
-                    operand +=number;
+                    operand += number;
                     break;
                 case "-":
-                    operand -=number;
+                    operand -= number;
                     break;
             }
         }
         resultField.setText(operand.toString().replace('.', ','));
         numberField.setText("");
     }
+
+
+
 }
